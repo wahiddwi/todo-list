f48397e8d5cb30f80e4e3c44d718ff6d06319cb8
wahiddwi <wahiddwi7@gmail.com>
2023-06-01 15:15:52 +0700

    todo-list

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,16 +1,26 @@
 import './Todo.css';
+import {useState} from 'react';
 import TodoList from '../todo-list/TodoList';
+import TodoCreate from '../todo-create/TodoCreate';
 
 const Todo = () => {
-    const todos = [
+    const [getTodos, setTodos] = useState([
         {id: 1, title: 'Eat'},
         {id: 2, title: 'Sleep'},
         {id: 3, title: 'Code'},
-    ];
+    ]);
+
+    const eventCreateTodo = (todo) => {
+        setTodos(getTodos.concat(todo));
+
+        console.log(getTodos);
+    }
+
     return (
         <div>
             <h3>Todo List</h3>
-            <TodoList todos={todos} />
+            <TodoCreate onCreateTodo={eventCreateTodo} />
+            <TodoList todos={getTodos} />
         </div>
     );
 }
